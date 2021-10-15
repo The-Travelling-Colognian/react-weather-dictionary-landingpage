eeeb9de1197932aea7c45df4e9580fb0bbbf4ec9
Vanessa Schade <82608228+travellingcolognian@users.noreply.github.com>
2021-10-15 20:36:35 +0200

    Created the FinalProjects component

---
+++ src/FinalProjects.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function FinalProjects() {
+  return (
+    <div className="FinalProjects">
+      <div className="row">
+        <div className="col-6">
+          <a
+            className="weatherApp btn btn-branding"
+            href="https://competent-almeida-bddaf5.netlify.app/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            React Weather App
+          </a>
+        </div>
+        <div className="col-6">
+          <a
+            className="weatherApp btn btn-branding"
+            href="https://infallible-visvesvaraya-096ea3.netlify.app/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            React Dictionary App
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
